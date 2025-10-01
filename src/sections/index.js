096425d8c7bece0708a5f5b5463b04ac333079bb
TsoyIgor <cd>
2025-10-01 23:28:19 +0400

    created nav and hero section

---
+++ src/sections/index.js
@@ -0,0 +1,12 @@
+import Hero from './Hero'
+import Popular from './Popular'
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import CustomerReviews from './CustomerReviews'
+import Subscribe from './Subscribe'
+import Footer from './Footer'
+
+export {
+    Hero, Popular, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer
+}
